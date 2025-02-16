14e30ea81ce7a446f59fc31be60927826c8f84d5
Cz <celenzopeuch@gmail.com>
2025-02-16 13:12:12 +0100

    Update fxmanifest.lua

--- Cz_gofast/fxmanifest.lua
+++ Cz_gofast/fxmanifest.lua
@@ -2,25 +2,24 @@ fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
 
-author 'VotreNom' -- Remplacez par votre nom
+author 'Cz' 
 description 'CZP GoFast Script'
 version '1.0.0'
 
--- Scripts côté client
+
 client_scripts {
     'client/Mission1.lua',
 }
 
--- Scripts côté serveur
+
 server_scripts {
     'server/server.lua'
 }
 
 shared_script '@es_extended/imports.lua'
--- Fichiers HTML (UI)
+
 ui_page 'html/index.html'
 
--- Fichiers à inclure pour l'interface utilisateur
 files {
     'html/index.html',
     'html/css/styles.css',
